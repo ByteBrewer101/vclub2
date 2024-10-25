bba2df1580d3bfaebb09683bc4c27021aca30ebd
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-10-25 21:00:29 +0530

    new server

--- src/Room.ts
+++ src/Room.ts
@@ -1,19 +1,66 @@
+// import WebSocket from "ws";
+// import { formatAndSendMessage } from "./Messagetypes";
+
+// export class Room {
+//   public user1: WebSocket;
+//   public user2: WebSocket;
+
+//   constructor(ws1: WebSocket, ws2: WebSocket) {
+//     this.user1 = ws1;
+//     this.user2 = ws2;
+//   }
+
+//   public sendMessage(sender: WebSocket, msg: string) {
+//     if (this.user1 === sender) {
+//       formatAndSendMessage(this.user2, msg, "user 2");
+//     } else formatAndSendMessage(this.user1, msg, "user 1");
+//   }
+
+//   public broadcastInRoom(msg: string) {
+//     formatAndSendMessage(this.user1, msg, "system");
+//     formatAndSendMessage(this.user2, msg, "system");
+//   }
+
+//   public removeUser() {
+//     //remove user
+//   }
+
+//   public checkExistance (ws:WebSocket){
+//     if(this.user1 === ws || this.user2 === ws)
+//     {
+//       return true
+//     }
+//     return false
+//   }
+
+// }
 import WebSocket from "ws";
 import { formatAndSendMessage } from "./Messagetypes";
 
 export class Room {
   public user1: WebSocket;
   public user2: WebSocket;
+  private username1: string;
+  private username2: string;
 
-  constructor(ws1: WebSocket, ws2: WebSocket) {
+  constructor(
+    ws1: WebSocket,
+    ws2: WebSocket,
+    username1: string,
+    username2: string
+  ) {
     this.user1 = ws1;
     this.user2 = ws2;
+    this.username1 = username1;
+    this.username2 = username2;
   }
 
   public sendMessage(sender: WebSocket, msg: string) {
     if (this.user1 === sender) {
-      formatAndSendMessage(this.user2, msg, "user 2");
-    } else formatAndSendMessage(this.user1, msg, "user 1");
+      formatAndSendMessage(this.user2, msg, this.username1);
+    } else {
+      formatAndSendMessage(this.user1, msg, this.username2);
+    }
   }
 
   public broadcastInRoom(msg: string) {
@@ -21,16 +68,7 @@ export class Room {
     formatAndSendMessage(this.user2, msg, "system");
   }
 
-  public removeUser() {
-    //remove user
+  public checkExistance(ws: WebSocket) {
+    return this.user1 === ws || this.user2 === ws;
   }
-
-  public checkExistance (ws:WebSocket){
-    if(this.user1 === ws || this.user2 === ws)
-    {
-      return true
-    }
-    return false
-  }
-
 }
--- src/RoomManager.ts
+++ src/RoomManager.ts
@@ -1,3 +1,165 @@
+// import { Chat, formatAndSendMessage, JoinRoom } from "./Messagetypes";
+// import { Room } from "./Room";
+// import WebSocket from "ws";
+// import { User } from "./User";
+
+// export class RoomManager {
+//   private Rooms: Room[];
+//   private lobby: WebSocket[];
+//   private users: User[];
+
+//   constructor() {
+//     this.Rooms = [];
+//     this.lobby = [];
+//     this.users = [];
+//   }
+
+//   addUser(ws: WebSocket, username:string) {
+//     console.log(`Adding user to the manager: ${ws}`);
+   
+//     const currentUser = new User(username,ws)
+
+//     this.users.push(currentUser);
+//     // const currentUser = new User
+//     this.addHandler(ws);
+//   }
+
+//   removeUser(ws: WebSocket) {
+//     console.log(`Removing user: ${ws}`);
+//     const currentRoom = this.findUserRoom(ws);
+
+//     if (currentRoom) {
+//       this.handleRoomExit(ws, currentRoom);
+//     } else {
+//       this.removeFromLobby(ws);
+//     }
+
+//     this.users = this.filterUser(ws, this.users);
+//   }
+
+//   private addHandler(ws: WebSocket) {
+//     ws.on("message", (data: string) => {
+//       const currentMessage = JSON.parse(data);
+//       console.log(`Received message: ${JSON.stringify(currentMessage)}`);
+
+//       if (currentMessage.type === JoinRoom) {
+//         this.JoinLogic(ws);
+//       }
+
+//       if (currentMessage.type === Chat) {
+//         this.handleChatMessage(ws, currentMessage.content);
+//       }
+//     });
+//   }
+
+//   private JoinLogic(ws: WebSocket) {
+//     this.disbandRoom(ws);
+//     this.sendSystemMessage(ws,"Waiting for someone to join")
+
+//     if (this.isInLobby(ws)) {
+//       return;
+//     }
+
+//     if (this.lobbyHasUsers(ws)) {
+//       const partner = this.lobby.shift();
+//       this.createRoom(ws, partner);
+//     } else {
+//       this.addToLobby(ws);
+//     }
+//   }
+
+//   private disbandRoom(ws: WebSocket) {
+//     const roomToDisband = this.findUserRoom(ws);
+
+//     if (roomToDisband) {
+//       this.handleRoomDisband(ws, roomToDisband);
+//     }
+//   }
+
+//   // Helper Functions
+
+//   private findUserRoom(ws: WebSocket): Room | undefined {
+//     return this.Rooms.find((room) => room.checkExistance(ws));
+//   }
+
+//   private handleRoomExit(ws: WebSocket, currentRoom: Room) {
+//     console.log(`User ${ws} is part of a room. Notifying partner.`);
+//     const otherUser = this.getOtherUser(ws, currentRoom);
+//     this.sendSystemMessage(otherUser, "Your partner has disconnected");
+
+//     console.log(`Moving ${otherUser} back to the lobby.`);
+//     this.addToLobby(otherUser);
+
+//     this.removeRoom(currentRoom);
+//   }
+
+//   private handleChatMessage(ws: WebSocket, messageContent: string) {
+//     const currentRoom = this.findUserRoom(ws);
+//     if (currentRoom) {
+//       currentRoom.sendMessage(ws, messageContent);
+//     } else {
+//       console.log(`User ${ws} is not in a room.`);
+//     }
+//   }
+
+//   private handleRoomDisband(ws: WebSocket, roomToDisband: Room) {
+//     console.log(`Disbanding room for user ${ws}.`);
+//     const otherUser = this.getOtherUser(ws, roomToDisband);
+
+//     roomToDisband.broadcastInRoom("Looking for a new  match")
+
+
+   
+
+//     this.addToLobby(otherUser);
+//     this.removeRoom(roomToDisband);
+
+//     console.log(`Room disbanded, moving partner to the lobby.`);
+//   }
+
+//   private removeFromLobby(ws: WebSocket) {
+//     console.log(`User ${ws} is in the lobby. Removing from lobby.`);
+//     this.lobby = this.filterUser(ws, this.lobby);
+//   }
+
+//   private addToLobby(ws: WebSocket) {
+//     this.lobby.push(ws);
+//   }
+
+//   private removeRoom(room: Room) {
+//     this.Rooms = this.Rooms.filter((r) => r !== room);
+//   }
+
+//   private lobbyHasUsers(ws: WebSocket): boolean {
+//     return this.lobby.length >= 1 && this.lobby[0] !== ws;
+//   }
+
+//   private isInLobby(ws: WebSocket): boolean {
+//     return this.lobby.includes(ws);
+//   }
+
+//   private createRoom(ws: WebSocket, partner: WebSocket | undefined) {
+//     if (partner) {
+//       const currentRoom = new Room(ws, partner);
+//       this.Rooms.push(currentRoom);
+//       currentRoom.broadcastInRoom("connected");
+//     }
+//   }
+
+//   private getOtherUser(ws: WebSocket, room: Room): WebSocket {
+//     return room.user1 === ws ? room.user2 : room.user1;
+//   }
+
+//   private sendSystemMessage(ws: WebSocket, message: string) {
+//     formatAndSendMessage(ws, message, "system");
+//   }
+
+//   private filterUser(ws: WebSocket, list: WebSocket[]): WebSocket[] {
+//     return list.filter((user) => user !== ws);
+//   }
+// }
+
+
 import { Chat, formatAndSendMessage, JoinRoom } from "./Messagetypes";
 import { Room } from "./Room";
 import WebSocket from "ws";
@@ -5,7 +167,7 @@ import { User } from "./User";
 
 export class RoomManager {
   private Rooms: Room[];
-  private lobby: WebSocket[];
+  private lobby: User[];
   private users: User[];
 
   constructor() {
@@ -14,18 +176,15 @@ export class RoomManager {
     this.users = [];
   }
 
-  addUser(ws: WebSocket, username:string) {
-    console.log(`Adding user to the manager: ${ws}`);
-   
-    const currentUser = new User(username,ws)
-
+  addUser(ws: WebSocket, username: string) {
+    console.log(`Adding user to the manager: ${username}`);
+    const currentUser = new User(username, ws);
     this.users.push(currentUser);
-    // const currentUser = new User
     this.addHandler(ws);
   }
 
   removeUser(ws: WebSocket) {
-    console.log(`Removing user: ${ws}`);
+    console.log(`Removing user: ${this.getUserByWs(ws)?.username}`);
     const currentRoom = this.findUserRoom(ws);
 
     if (currentRoom) {
@@ -34,7 +193,7 @@ export class RoomManager {
       this.removeFromLobby(ws);
     }
 
-    this.users = this.filterUser(ws, this.users);
+    this.users = this.users.filter((user) => user.ws !== ws);
   }
 
   private addHandler(ws: WebSocket) {
@@ -54,7 +213,7 @@ export class RoomManager {
 
   private JoinLogic(ws: WebSocket) {
     this.disbandRoom(ws);
-    this.sendSystemMessage(ws,"Waiting for someone to join")
+    this.sendSystemMessage(ws, "Waiting for someone to join");
 
     if (this.isInLobby(ws)) {
       return;
@@ -62,7 +221,9 @@ export class RoomManager {
 
     if (this.lobbyHasUsers(ws)) {
       const partner = this.lobby.shift();
-      this.createRoom(ws, partner);
+      if (partner) {
+        this.createRoom(ws, partner.ws);
+      }
     } else {
       this.addToLobby(ws);
     }
@@ -82,13 +243,24 @@ export class RoomManager {
     return this.Rooms.find((room) => room.checkExistance(ws));
   }
 
-  private handleRoomExit(ws: WebSocket, currentRoom: Room) {
-    console.log(`User ${ws} is part of a room. Notifying partner.`);
-    const otherUser = this.getOtherUser(ws, currentRoom);
-    this.sendSystemMessage(otherUser, "Your partner has disconnected");
+  private getUserByWs(ws: WebSocket): User | undefined {
+    return this.users.find((user) => user.ws === ws);
+  }
 
-    console.log(`Moving ${otherUser} back to the lobby.`);
-    this.addToLobby(otherUser);
+  private handleRoomExit(ws: WebSocket, currentRoom: Room) {
+    const otherWs = this.getOtherUser(ws, currentRoom);
+    const otherUser = this.getUserByWs(otherWs);
+
+    if (otherUser) {
+      console.log(
+        `User ${
+          this.getUserByWs(ws)?.username
+        } is leaving room. Notifying partner ${otherUser.username}.`
+      );
+      this.sendSystemMessage(otherWs, "Your partner has disconnected");
+      console.log(`Moving ${otherUser.username} back to the lobby.`);
+      this.addToLobby(otherWs);
+    }
 
     this.removeRoom(currentRoom);
   }
@@ -98,32 +270,41 @@ export class RoomManager {
     if (currentRoom) {
       currentRoom.sendMessage(ws, messageContent);
     } else {
-      console.log(`User ${ws} is not in a room.`);
+      console.log(`User ${this.getUserByWs(ws)?.username} is not in a room.`);
     }
   }
 
   private handleRoomDisband(ws: WebSocket, roomToDisband: Room) {
-    console.log(`Disbanding room for user ${ws}.`);
-    const otherUser = this.getOtherUser(ws, roomToDisband);
+    const otherWs = this.getOtherUser(ws, roomToDisband);
+    const otherUser = this.getUserByWs(otherWs);
 
-    roomToDisband.broadcastInRoom("Looking for a new  match")
+    if (otherUser) {
+      console.log(
+        `Disbanding room for user ${this.getUserByWs(ws)?.username}.`
+      );
+      roomToDisband.broadcastInRoom("Looking for a new match");
+      this.addToLobby(otherWs);
+    }
 
-
-   
-
-    this.addToLobby(otherUser);
     this.removeRoom(roomToDisband);
-
     console.log(`Room disbanded, moving partner to the lobby.`);
   }
 
   private removeFromLobby(ws: WebSocket) {
-    console.log(`User ${ws} is in the lobby. Removing from lobby.`);
-    this.lobby = this.filterUser(ws, this.lobby);
+    const user = this.getUserByWs(ws);
+    if (user) {
+      console.log(
+        `User ${user.username} is in the lobby. Removing from lobby.`
+      );
+      this.lobby = this.lobby.filter((lobbyUser) => lobbyUser.ws !== ws);
+    }
   }
 
   private addToLobby(ws: WebSocket) {
-    this.lobby.push(ws);
+    const user = this.getUserByWs(ws);
+    if (user) {
+      this.lobby.push(user);
+    }
   }
 
   private removeRoom(room: Room) {
@@ -131,16 +312,19 @@ export class RoomManager {
   }
 
   private lobbyHasUsers(ws: WebSocket): boolean {
-    return this.lobby.length >= 1 && this.lobby[0] !== ws;
+    return this.lobby.length >= 1 && this.lobby[0].ws !== ws;
   }
 
   private isInLobby(ws: WebSocket): boolean {
-    return this.lobby.includes(ws);
+    return this.lobby.some((user) => user.ws === ws);
   }
 
-  private createRoom(ws: WebSocket, partner: WebSocket | undefined) {
-    if (partner) {
-      const currentRoom = new Room(ws, partner);
+  private createRoom(ws: WebSocket, partner: WebSocket) {
+    const user1 = this.getUserByWs(ws);
+    const user2 = this.getUserByWs(partner);
+
+    if (user1 && user2) {
+      const currentRoom = new Room(ws, partner, user1.username, user2.username);
       this.Rooms.push(currentRoom);
       currentRoom.broadcastInRoom("connected");
     }
@@ -152,9 +336,5 @@ export class RoomManager {
 
   private sendSystemMessage(ws: WebSocket, message: string) {
     formatAndSendMessage(ws, message, "system");
-  }
-
-  private filterUser(ws: WebSocket, list: WebSocket[]): WebSocket[] {
-    return list.filter((user) => user !== ws);
   }
 }
