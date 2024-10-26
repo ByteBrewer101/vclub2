233f9bf4f47d1739f4de3a7008a0fe4ff6a77103
ByteBrewer101 <161151563+ByteBrewer101@users.noreply.github.com>
2024-10-27 04:57:01 +0530

    latest

--- src/Messagetypes.ts
+++ src/Messagetypes.ts
@@ -1,20 +1,3 @@
-// import WebSocket from "ws";
-// export const JoinRoom = "joinRoom";
-// export const Chat = "chat";
-
-// export function formatAndSendMessage(
-//   ws: WebSocket,
-//   msg: string,
-//   sender: string
-// ) {
-//   const finalMsg = JSON.stringify({
-//     sender: sender,
-//     msg: msg,
-//   });
-
-//   ws.send(finalMsg);
-// }
-
 
 import WebSocket from "ws";
 
--- src/Room.ts
+++ src/Room.ts
@@ -1,39 +1,4 @@
-// import WebSocket from "ws";
-// import { formatAndSendMessage } from "./Messagetypes";
 
-// export class Room {
-//   public user1: WebSocket;
-//   public user2: WebSocket;
-
-//   constructor(ws1: WebSocket, ws2: WebSocket) {
-//     this.user1 = ws1;
-//     this.user2 = ws2;
-//   }
-
-//   public sendMessage(sender: WebSocket, msg: string) {
-//     if (this.user1 === sender) {
-//       formatAndSendMessage(this.user2, msg, "user 2");
-//     } else formatAndSendMessage(this.user1, msg, "user 1");
-//   }
-
-//   public broadcastInRoom(msg: string) {
-//     formatAndSendMessage(this.user1, msg, "system");
-//     formatAndSendMessage(this.user2, msg, "system");
-//   }
-
-//   public removeUser() {
-//     //remove user
-//   }
-
-//   public checkExistance (ws:WebSocket){
-//     if(this.user1 === ws || this.user2 === ws)
-//     {
-//       return true
-//     }
-//     return false
-//   }
-
-// }
 import WebSocket from "ws";
 import { formatAndSendMessage } from "./Messagetypes";
 
